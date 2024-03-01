8949f322afc7e229cd452380c29aa62d971054f9
SLASH-7 <basnetujjwal98@gmail.com>
2024-03-01 17:16:23 +0545

    complete button

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
             <h3 className="text-[#c3c0c0] montserrat font-semibold text-2xl md:pb-7 pb-5">
               <span ref={typedRef}></span>
             </h3>
-            <button className="main-theme-bg rounded-3xl uppercase text-[12px] tracking-[1px] font-[600] text-white px-7 py-3">
+            <button className="main-theme-bg rounded-3xl uppercase text-[12px] tracking-[1px] font-[600] text-white px-9 py-3 fill-up-button">
               Hire Me
             </button>
           </div>
