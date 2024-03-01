503c8f88f78369483259ed0d5b1d110bb2f1ad95
SLASH-7 <basnetujjwal98@gmail.com>
2024-03-01 17:03:50 +0545

    complete nav and home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,43 @@
-import React from "react";
-
+import React, { useRef, useEffect } from "react";
+import { Typed } from "react-typed";
 const Home = () => {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    if (typedRef.current) {
+      const typed = new Typed(typedRef.current, {
+        strings: ["I'm a Web Developer", "I Love Software Development"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true,
+      });
+      return () => {
+        typed.destroy();
+      };
+    }
+  }, []);
   return (
-    <section className="home">
-      <div className="overlay w-full h-screen absolute top-0 left-0"></div>
-    </section>
+    <>
+      <section className="home">
+        <div className="overlay w-full h-screen flex items-center">
+          <div className="container px-2">
+            <h3 className="text-[#c3c0c0] montserrat font-semibold text-2xl">
+              Hey, My Name Is
+            </h3>
+            <h1 className="lg:text-6xl sm:text-5xl text-[45px] font-[800] text-white md:py-7 py-5">
+              Ujjwal Basnet
+            </h1>
+            <h3 className="text-[#c3c0c0] montserrat font-semibold text-2xl md:pb-7 pb-5">
+              <span ref={typedRef}></span>
+            </h3>
+            <button className="main-theme-bg rounded-3xl uppercase text-[12px] tracking-[1px] font-[600] text-white px-7 py-3">
+              Hire Me
+            </button>
+          </div>
+        </div>
+      </section>
+      <div className="section-two h-screen"></div>
+    </>
   );
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import cv from "../assets/cv.jpg";
 import html2pdf from "html2pdf.js";
 
 const Nav = () => {
+  const [scroll, setScroll] = useState(0);
+  const [color, setColor] = useState(false);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+  window.addEventListener("scroll", handleScroll);
   // download pdf
   const pdfDownload = () => {
     const itemsPdf = document.getElementById("cv");
@@ -25,11 +32,14 @@ const Nav = () => {
     html2pdf().from(itemsPdf).set(pdfOptions).save();
   };
   return (
-    <header className="nav">
-      <nav className="container primary-color flex-pack px-2 h-16">
-        <h1 className=" text-2xl font-semibold">Ujjwal Basnet</h1>
+    <header className={`${scroll > 190 ? "nav-after" : "nav"} fixed w-full`}>
+      <nav className="container primary-color flex-pack px-2 h-14">
+        <h1 className="montserrat md:text-2xl text-xl font-semibold">
+          <span className="main-theme-text">U</span>
+          jjwal Basnet
+        </h1>
         <button
-          className="section rounded-lg p-2 border-custom"
+          className="section rounded-lg p-2 text-sm border-custom"
           onClick={pdfDownload}
         >
           Download Resume
